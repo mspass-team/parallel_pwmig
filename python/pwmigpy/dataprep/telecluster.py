ff48fd7061641657cb6f5a8fe1b205e2131e377b
Gary Pavlis <pavlis@indiana.edu>
2025-09-23 05:24:45 -0400

    Revise teleluster.py to be suitable as a command line tool

--- python/pwmigpy/dataprep/telecluster.py
+++ python/pwmigpy/dataprep/telecluster.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 # temporary to use sys.path
 import sys
+import argparse
+import json
 from pwmigpy.ccore.seispp import (EventCatalog,Hypocenter,RadialGrid,SectorTest)
 from mspasspy.ccore.utility import (Metadata,AntelopePf)
 from mspasspy.db.database import Database
-from mspasspy.db.client import Client
+from mspasspy.db.client import DBClient
 import numpy as np
 
 def dbload_EventCatalog(db,mdlist=[],query={}):
@@ -143,7 +145,7 @@ def telecluster(dbname,pfname="telecluster.pf",query={},othermd=[]):
     gridname=pf.get_string('gridname')
     grid=pfload_radial_grid(pf)
     # Now attempt to load the source data
-    dbclient=Client()
+    dbclient=DBClient()
     db=Database(dbclient,dbname)
     cluster_collection=db['telecluster']
     evcat=dbload_EventCatalog(db,mdlist=othermd,query=query)
@@ -174,6 +176,73 @@ def telecluster(dbname,pfname="telecluster.pf",query={},othermd=[]):
                 doc['gridcell']=cellsubdoc
                 print(doc)
                 cluster_collection.insert_one(doc)
-
-telecluster('usarraytest')          
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    parser = argparse.ArgumentParser(
+        prog = "telecluster",
+        usage="%(prog)s dbname [-pf pffile -q source_query -o keylist]",
+        description="Populate cluster collection for source data",
+    )
+    parser.add_argument(
+        "dbname",
+        metavar="dbname",
+        type=str,
+        help="MongoDB database name containg source collection to be processed",
+    )
+    parser.add_argument(
+        "-pf",
+        "pffile",
+        action="store",
+        type=str,
+        default="Telecluster.pf",
+        help="Set parameter file that sets up clustering geometry ",
+    )
+    parser.add_argument(
+        "-q",
+        "--query",
+        action="store",
+        type=str,
+        default=None,
+        help="Specify optional query (json format) to apply to source collection",
+    )
+    parser.add_argument(
+        "-o",
+        "--otherdata",
+        action="store",
+        type=str,
+        default=None,
+        help="Use with comma separated list of keys for other data to be loaded from source and stored in cluster collection"
+    )
+    args = parser.parse_args(args)
+    # maybe should alter the function to take the mongodb handle not just 
+    # the name.   Only issue is exception handling but for now leave  
+    # as is - if it ain't broken don't fix it
+    dbname = args.dbname
+    # The telecluster functionis implemented to use the pfname not the 
+    # object constructed from it
+    pffile = args.pffile
+    # depend on None as default
+    if args.query is None:
+        query = dict()
+    else:
+        try:
+            query = json.loads(args.query)
+        except json.decoder.JSONDecodeError as e:
+            print("telecluster:  Error in json specification of query operator with -q option")
+            print("string received=",args.query)
+            print("Exception message from json module:")
+            print(e)
+            exit(-1)
+    if args.otherdata is None:
+        otherdata = []
+    else:
+        otherdata = args.otherdata.split(",")
+    # this main function could throw exceptions but we don't try to 
+    # handlle them.  If I (glp) find them too cryptic could put an error 
+    # hander here
+    telecluster(dbname,pfname=pffile,query=query,othermd=otherdata)
     
+    
+if __name__ == "__main__":
+    main()
