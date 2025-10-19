6b5b05897d97957198ae65ee30d7ae479486e807
Gary Pavlis <pavlis@indiana.edu>
2025-10-19 05:25:00 -0400

    Changes to cmake configuration for comptability with mspass changes

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.4)
+cmake_minimum_required(VERSION 3.11)
 
 project(boost-download NONE)
 
@@ -8,11 +8,11 @@ ExternalProject_Add(
   boost
   SOURCE_DIR "@BOOST_DOWNLOAD_ROOT@/boost-src"
   URL
-    https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.gz
+    https://archives.boost.io/release/1.86.0/source/boost_1_86_0.tar.gz
   URL_HASH
-    SHA256=96b34f7468f26a141f6020efb813f1a2f3dfb9797ecf76a7d7cbd843cc95f5bd
-  UPDATE_COMMAND wget https://svn.boost.org/trac10/raw-attachment/ticket/11120/python_jam.patch && patch tools/build/src/tools/python.jam python_jam.patch
-  CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}
+    SHA256=2575e74ffc3ef1cd0babac2c1ee8bdb5782a0ee672b1912da40e5b4b591ca01f
+
+  CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR} --with-toolset=gcc
   BUILD_COMMAND ./b2 cxxflags=-fPIC --with-serialization -j 8
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND ./b2 install
