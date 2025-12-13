fcc96e9bb52467bb2d4ae2d5095aee5ad729ece8
Gary Pavlis <pavlis@indiana.edu>
2025-12-13 08:09:46 -0500

    Add verbose debug scaffolding to migrate_one_seismogram funcion

--- cxx/src/lib/pwmigcore/migrate_one_seismogram.cc
+++ cxx/src/lib/pwmigcore/migrate_one_seismogram.cc
@@ -47,6 +47,8 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
     deadguy.live=false;
     return deadguy;
   }
+  std::cout << "Debug migrate_one_seismogram:  "
+  << " pwdata.live="<<pwdata.live()<<std::endl;
   /* These two metadata gets are closely linked to a map function used
   in the python functoin that calls this one.   The names here must match
   that functions usage or we will surely abort.*/
@@ -116,6 +118,8 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
   /* We create the output now so we can post error messages to the
   elog of this object that is what we return*/
   PWMIGmigrated_seismogram result(i,j,n3);
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "result created with n3="<<n3<<std::endl;
   // We need a reference ray for the incident wavefield.
   // This ray is chopped up and transformed to GCLgrid coordinates
   // in the integration loop below.  We compute it here to
@@ -129,12 +133,16 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
   // current seismogram (i,j).  First we compute the S wave
   // travel time down this ray path (i.e. ordered from surface down)
   Stime = compute_Stime(Us3d,i,j,raygrid,use_3d_vmodel);
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "compute_Stime return vector of length="<<Stime.size()<<endl;
   // Now we compute the gradient in the S ray travel time
   // for each point on the ray.  Could have been done with
   // less memory use in the loop below, but the simplification
   // it provides seems useful to me.  Note this is a 3xn3 matrix
   // like gradTp that is filled inside the loop
   dmatrix gradTs=compute_gradS(raygrid,i,j,Vs1d);
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "Number of columns in matrix returned by compute_gradS="<<gradTs.columns()<<std::endl;
   dmatrix gradTp(3,n3);
   dmatrix nup(3,n3);
   vector<double> zP(n3);
@@ -242,6 +250,8 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
     tlag=Tpx+Stime[k]+tdelta-Tpr;
     SPtime.push_back(tlag);
   }
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "Size of SPtime vector computed="<<SPtime.size()<<std::endl;
 
   // skip this ray if there were travel time computation problems
   if(tcompute_problem)
@@ -317,6 +327,8 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
   //
   if(rcomp_wt || !weight_functions_set)
   {
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "Entered weight computation block with stack_only="<<stack_only<<endl;
     /* This is a gross inefficiency in stack_only but since I only expect it to be
     used for CCP stacking equivalent, this should not be a big deal.  Probably should
     do it right some day */
@@ -333,7 +345,12 @@ PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwdata,
         raygrid, i, j, gradTp,zP);
     }
     if(use_grt_weights && (!stack_only) )
+    {
+  std::cout << "Debug migrate_one_seismogram:  "
+  << "Calling compute_weight_for_path gradTP and gradTs sizes"
+  << gradTp.columns()<<" "<<gradTs.columns()<<std::endl;
       result.dweight=compute_weight_for_path(gradTp,gradTs);
+    }
     else
       for(k=0;k<n3;++k)result.dweight[k]=1.0;
     if(smooth_wt && (!stack_only))
